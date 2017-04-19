3c8bb5715766e31ba1d77441681e9d59c57b6dbe
System <none>
2017-04-19 16:40:19 +0000

    🚘💐init commit from Glitch

---
+++ emailer.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+const {logger} = require('./utilities/logger');
+
+// stored in `.env` -- never store passwords, api keys
+// etc. inside source code
+const {SMTP_URL} = process.env;
+
+const emailData = {
+  from: 'jeremy.webdevelopment@gmail.com',
+  to: 'jeremy.m.sutton@gmail.com',
+  subject: 'Server Error Challenge',
+  text: "There was an error thrown.",
+  html: "<h1>Successful Error Thrown</h1>"
+}
+
+// `emailData` is an object that looks like this:
+// {
+//  from: "foo@bar.com",
+//  to: "bizz@bang.com, marco@polo.com",
+//  subject: "Hello world",
+//  text: "Plain text content",
+//  html: "<p>HTML version</p>"
+// }
+const sendEmail = (emailData, smtpUrl=SMTP_URL) => {
+  const transporter = nodemailer.createTransport(SMTP_URL);
+  logger.info(`Attempting to send email from ${emailData.from}`);
+  return transporter
+    .sendMail(emailData)
+    .then(info => console.log(`Message sent: ${info.response}`))
+    .catch(err => console.log(`Problem sending email: ${err}`));
+}
+
+
+module.exports = {sendEmail};
